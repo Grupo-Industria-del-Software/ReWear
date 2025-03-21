7c970345a36aa20bc4479afdefae38ba7e0ed5ae
SebastianHernandez69 <sh3641046@gmail.com>
2025-03-20 18:46:58 -0600

    feat: agregando entidades del aggregate de productos

--- Domain/AggregateRoots/Products/Product.cs
+++ Domain/AggregateRoots/Products/Product.cs
@@ -1,16 +1,34 @@
 using Domain.Common;
+using Domain.Entities;
 
 namespace Domain.AggregateRoots.Products;
 
 public class Product : AggregateRoot
 {
     public int UserId { get; set; }
+    public User? User { get; set; }
+    
     public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+    
     public int ConditionId { get; set; }
-    public int StatusId { get; set; }
+    public Condition? Condition { get; set; }
+    
+    public int SizeId { get; set; }
+    public Size? Size { get; set; }
+    
+    public int BrandId { get; set; }
+    public Brand? Brand { get; set; }
+    
+    public int ProductStatusId { get; set; }
+    public ProductStatus? ProductStatus { get; set; }
+    
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public bool IsForSale { get; set; }
+    public bool IsForRental { get; set; }
     public decimal Price { get; set; }
     public decimal RentalPrice { get; set; }
     
+    public List<ProductImage> ProductImages { get; set; } = new();
 }
