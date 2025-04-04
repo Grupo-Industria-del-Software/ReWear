4bd2c6759c191a56417601563cb88b0b7d5f2e86
SebastianHernandez69 <sh3641046@gmail.com>
2025-04-04 10:02:51 -0600

    Interfaces para obtener usuarios en admin

--- Application/Interfaces/Users/IUserService.cs
+++ Application/Interfaces/Users/IUserService.cs
@@ -6,6 +6,8 @@ namespace Application.Interfaces.Users;
 
 public interface IUserService
 {
+    Task<List<UserResponseDTO>> GetAllUsers();
+    Task<bool> ChageState(int userId);
     Task<bool> UpdateUser(int id, UserRequestDto dto);
     Task<UserResponseDTO?> GetByIdAsync(int id);
 }
