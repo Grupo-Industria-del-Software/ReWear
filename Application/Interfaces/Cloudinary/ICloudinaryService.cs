494dbe9a024b7003a2a613ccf0adf5e29e1407b6
José Alberto Ulloa Avila <ackertaker69@gmail.com>
2025-04-04 22:20:57 -0600

    Se cambio el dto de cloudinary a la carpeta de dtos

--- Application/Interfaces/Cloudinary/ICloudinaryService.cs
+++ Application/Interfaces/Cloudinary/ICloudinaryService.cs
@@ -1,7 +1,9 @@
+using Application.DTOs.Cloudinary;
+
 namespace Application.Interfaces.Cloudinary;
 
 public interface ICloudinaryService
 {
-    Task<CloudinaryUploadResult> UploadImageAsync(string imagePath);
+    Task<CloudinaryUploadResultDto> UploadImageAsync(string imagePath);
     Task<bool> DeleteImageAsync(string publicId);
 }
