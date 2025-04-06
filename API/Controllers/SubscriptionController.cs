4772b278c90afb3621e0bb8a72f4c53bce7a50bb
SebastianHernandez69 <sh3641046@gmail.com>
2025-04-06 16:26:27 -0600

    feat: agregando endpoints para obtener la subscription del usuario

--- API/Controllers/SubscriptionController.cs
+++ API/Controllers/SubscriptionController.cs
@@ -10,11 +10,31 @@ namespace API.Controllers;
 [Route("api/[controller]")]
 public class SubscriptionController : ControllerBase
 {
+    private readonly ISubscriptionService _subscriptionService;
+
+    public SubscriptionController(ISubscriptionService subscriptionService)
+    {
+        _subscriptionService = subscriptionService;
+    }
+    
     [Authorize]
     [HttpGet]
     [ServiceFilter(typeof(SubscriptionRequirementFilter))]
     public IActionResult GetSubscriptionActive()
     {
         return Ok();
+    }
+
+    [Authorize(Roles = "Seller")]
+    [HttpGet("info")]
+    public async Task<IActionResult> GetSubscriptionByUserId()
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        
+        if(userId == null)
+            return Unauthorized();
+        
+        var subscription = await _subscriptionService.GetByUserIdAsync(int.Parse(userId));
+        return subscription == null ? NotFound() : Ok(subscription);
     }
 }
